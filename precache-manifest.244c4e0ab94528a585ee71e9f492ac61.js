a49e728201734bc2f4e23c88db4a0bfaefc7a3ca
JMM-com <jalalmarzooq@gmail.com>
2020-07-18 14:11:03 +0400

    Updates

---
+++ precache-manifest.244c4e0ab94528a585ee71e9f492ac61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a1538c62e2969709d108228dc086ce",
+    "url": "/click/index.html"
+  },
+  {
+    "revision": "a539d835b21df0689a96",
+    "url": "/click/static/css/main.3019897f.chunk.css"
+  },
+  {
+    "revision": "4943669edaa3a77110b2",
+    "url": "/click/static/js/2.a1c6bf20.chunk.js"
+  },
+  {
+    "revision": "a539d835b21df0689a96",
+    "url": "/click/static/js/main.1ef96e1e.chunk.js"
+  },
+  {
+    "revision": "d5007d6430dea61d6907",
+    "url": "/click/static/js/runtime-main.b43bf87e.js"
+  }
+]);
